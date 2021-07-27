d401241adc62bda8946c0ac0043c8212caab098d
millertim83 <80060826+millertim83@users.noreply.github.com>
2021-07-27 10:02:28 -0400

    added JS functionality for inputting expense

---
+++ script.js
@@ -0,0 +1,63 @@
+const expenseTable = document.getElementById('expense-table');
+const submitButton = document.getElementById('submit-expense-button');   
+
+function addDate() {
+    const dateInput = document.getElementById('date-input');
+    const dateCell = dateInput.value;
+    return dateCell;
+}
+
+function addPrice() {
+    const priceInput = document.getElementById('expense-amount');
+    const priceCell = priceInput.value;
+    return priceCell;
+}
+
+function addExpenseDescription() {
+    const expenseDescription = document.getElementById('expense-description');
+    const expenseCell = expenseDescription.value;
+    return expenseCell;
+}
+
+function addPurchaseLocation() {
+    const purchaseLocation = document.getElementById('purchase-location');
+    const purchaseLocationCell = purchaseLocation.value;
+    return purchaseLocationCell;
+}
+
+function addPaymentMethod() {
+    const paymentMethod = document.getElementById('payment-method');
+    const paymentMethodCell = paymentMethod.value;
+    return paymentMethodCell;
+}
+
+submitButton.addEventListener('click', function addExpense(e) {
+    e.preventDefault();
+    const newRow = expenseTable.insertRow(1);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+    const cell4 = newRow.insertCell(3);
+    const cell5 = newRow.insertCell(4);
+
+    cell1.textContent = addDate();
+    cell2.textContent = addPrice();
+    cell3.textContent = addExpenseDescription();
+    cell4.textContent = addPurchaseLocation();
+    cell5.textContent = addPaymentMethod();
+});
+
+
+
+    
+
+        
+    
+ 
+
+
+    
+
+
+
+    
