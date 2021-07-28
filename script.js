103c262fd86ade03611d866c59935358b1d6a941
millertim83 <80060826+millertim83@users.noreply.github.com>
2021-07-28 14:04:38 -0400

    added delete button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const expenseTable = document.getElementById('expense-table');
-const submitButton = document.getElementById('submit-expense-button');   
+const submitButton = document.getElementById('submit-expense-button')
+const deleteButton = createDeleteButton();
+const form = document.getElementById('expense-input')
 
 function addDate() {
     const dateInput = document.getElementById('date-input');
@@ -31,33 +33,40 @@ function addPaymentMethod() {
     return paymentMethodCell;
 }
 
-submitButton.addEventListener('click', function addExpense(e) {
+submitButton.addEventListener('click', function addExpense(e) {        
     e.preventDefault();
+
     const newRow = expenseTable.insertRow(1);
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
     const cell3 = newRow.insertCell(2);
     const cell4 = newRow.insertCell(3);
     const cell5 = newRow.insertCell(4);
-
+    
     cell1.textContent = addDate();
     cell2.textContent = addPrice();
     cell3.textContent = addExpenseDescription();
     cell4.textContent = addPurchaseLocation();
     cell5.textContent = addPaymentMethod();
+
+    
+    
+    newRow.appendChild(deleteButton);
+    deleteButton.addEventListener('click', () => {
+        newRow.remove();
+    });
+    return deleteButton;
+
 });
 
-
-
-    
-
-        
-    
+function createDeleteButton(newRow) {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete expense";
+    return deleteButton;
+}
  
-
-
-    
-
-
-
-    
+ /*if (dateCell === '' || priceCell === '' || expenseCell === '' || 
+    purchaseLocationCell === '' || paymentMethodCell === '') {
+    alert('Please fill out all fields!');
+    return;
+    }*/    
