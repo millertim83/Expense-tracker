c55386ed0d69790c17453debfc32a4295954302c
millertim83 <80060826+millertim83@users.noreply.github.com>
2021-07-29 12:10:45 -0400

    fixed delete button functionality

--- script.js
+++ script.js
@@ -35,27 +35,29 @@ function addPaymentMethod() {
 
 submitButton.addEventListener('click', function addExpense(e) {        
     e.preventDefault();
-
-    const newRow = expenseTable.insertRow(1);
+    const newRow = expenseTable.insertRow(-1);
+    const deleteButton = createDeleteButton(newRow);
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
     const cell3 = newRow.insertCell(2);
     const cell4 = newRow.insertCell(3);
     const cell5 = newRow.insertCell(4);
     
+    
     cell1.textContent = addDate();
     cell2.textContent = addPrice();
     cell3.textContent = addExpenseDescription();
     cell4.textContent = addPurchaseLocation();
     cell5.textContent = addPaymentMethod();
+    
 
     
     
     newRow.appendChild(deleteButton);
-    deleteButton.addEventListener('click', () => {
+    deleteButton.addEventListener('click', (e) => {
         newRow.remove();
     });
-    return deleteButton;
+    
 
 });
 
@@ -64,9 +66,4 @@ function createDeleteButton(newRow) {
     deleteButton.textContent = "Delete expense";
     return deleteButton;
 }
- 
- /*if (dateCell === '' || priceCell === '' || expenseCell === '' || 
-    purchaseLocationCell === '' || paymentMethodCell === '') {
-    alert('Please fill out all fields!');
-    return;
-    }*/    
+  
